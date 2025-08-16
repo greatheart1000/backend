69ee713128ee2b19793750e17ffdd157ef686041
greatheart1000 <greatheart1000@163.com>
2025-08-16 15:23:56 +0800

    Update main.go

--- chatper10/main.go
+++ chatper10/main.go
@@ -23,8 +23,27 @@ func foo1() int {
 	return *x
 }
 
-func main() {
+type data struct {
+	name string
+}
 
+func getData() data {
+	p := data{name: "zhangsan"}
+	return p
+}
+
+// 返回结构体指针
+func getData1() *data {
+	p := data{name: "李四"}
+	return &p
+}
+
+func main() {
+	x := getData()
+	fmt.Println("getData函数调用:" + x.name)
+
+	x1 := getData1()
+	fmt.Printf("函数2返回的值:%p", x1)
 	p := foo()
 	fmt.Printf("foo 返回的指针: %p，解引用后的值: %d\n", p, *p)
 
